1c73c06adc67d118cf9caf4fa58b3594e1eb9f06
Parker Rex <me@parkerrex.com>
2025-07-01 15:32:57 -0400

    fix size

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -1,27 +1,33 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { SunIcon } from "@/components/ui/sun"
-import { MoonIcon } from "@/components/ui/moon"
-import { useTheme } from "next-themes"
+import * as React from "react";
+import { SunIcon } from "@/components/ui/sun";
+import { MoonIcon } from "@/components/ui/moon";
+import { useTheme } from "next-themes";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+} from "@/components/ui/dropdown-menu";
 
 export function ThemeToggle() {
-  const { setTheme } = useTheme()
+  const { setTheme } = useTheme();
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon" className="relative">
-          <SunIcon size={19} className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <MoonIcon size={19} className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <Button variant="outline" className="relative h-1">
+          <SunIcon
+            size={3}
+            className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+          />
+          <MoonIcon
+            size={3}
+            className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+          />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
@@ -37,5 +43,5 @@ export function ThemeToggle() {
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
-  )
-}    
+  );
+}
