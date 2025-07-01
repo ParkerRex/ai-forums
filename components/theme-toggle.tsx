35009d090a3180b00ea123d657b16c20e4c05f6e
Parker Rex <me@parkerrex.com>
2025-07-01 15:42:44 -0400

    fix system icon

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -3,6 +3,7 @@
 import * as React from "react";
 import { SunIcon } from "@/components/ui/sun";
 import { MoonIcon } from "@/components/ui/moon";
+import { LaptopMinimalCheckIcon } from "@/components/ui/laptop-minimal-check";
 import { useTheme } from "next-themes";
 
 import { Button } from "@/components/ui/button";
@@ -14,18 +15,18 @@ import {
 } from "@/components/ui/dropdown-menu";
 
 export function ThemeToggle() {
-  const { setTheme } = useTheme();
+  const { setTheme, theme } = useTheme();
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" className="relative h-1">
+        <Button variant="ghost" size="icon" className="h-5 w-5 p-0">
           <SunIcon
-            size={3}
+            size={14}
             className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
           />
           <MoonIcon
-            size={3}
+            size={14}
             className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
           />
           <span className="sr-only">Toggle theme</span>
@@ -33,12 +34,15 @@ export function ThemeToggle() {
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuItem onClick={() => setTheme("light")}>
+          <SunIcon size={14} className="mr-2" />
           Light
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("dark")}>
+          <MoonIcon size={14} className="mr-2" />
           Dark
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme("system")}>
+          <LaptopMinimalCheckIcon size={14} className="mr-2" />
           System
         </DropdownMenuItem>
       </DropdownMenuContent>
