c7ee1d8519a4ad8b09c8f6feff4b2f455ed61d24
Parker Rex <me@parkerrex.com>
2025-07-21 19:56:55 -0400

    more ui

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -4,7 +4,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const badgeVariants = cva(
-  "inline-flex items-center rounded-none border px-2.5 py-0.5 text-xs font-mono font-semibold uppercase tracking-wide transition-colors focus:outline-none",
+  "inline-flex items-center rounded-none border px-2.5 py-0.5 text-xs font-mono font-semibold uppercase tracking-wide transition-colors focus:outline-none select-none",
   {
     variants: {
       variant: {
